249dadf39ba7245522bfcc96d7f338ac91ce9c10
tangrowth <to.chinatsu@icloud.com>
2023-12-16 15:32:57 +0900

    feat: ダミーデータの作成

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->safeEmail(),
+            'tell' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->optional()->sentence,
+            'detail' => $this->faker->realText(120),
+        ];
+    }
+}
